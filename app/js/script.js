e3b98eb98783b1aa3c7bfabab77fa4a9b10affa5
vetonshala <vetonnshala@gmail.com>
2021-01-27 17:01:28 +0100

    menu and mobile version

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,27 @@
-console.log("Starting");
+const btnHamburger = document.querySelector('#btnHamburger');
+const header = document.querySelector('.header');
+const overlay = document.querySelector('.overlay');
+const fadeElems = document.querySelectorAll('.has-fade')
+const body = document.querySelector('body');
+
+btnHamburger.addEventListener('click', function() {
+
+
+    if (header.classList.contains('open')) { //close hamburger menu
+        body.classList.remove('noscroll');
+        header.classList.remove('open');
+        fadeElems.forEach(function(element) {
+            element.classList.remove('fade-in');
+            element.classList.add('fade-out');
+        });
+    } else { //open hamburger menu 
+        body.classList.add('noscroll');
+        header.classList.add('open');
+        fadeElems.forEach(function(element) {
+            element.classList.remove('fade-out');
+            element.classList.add('fade-in');
+        });
+
+    }
+
+});
