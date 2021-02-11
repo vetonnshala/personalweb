28e3e34d82e5a9b05854da498ba6ff8544c1bbc8
vetonshala <vetonnshala@gmail.com>
2021-02-11 17:25:27 +0100

    Error Fix

--- app/js/script.js
+++ app/js/script.js
@@ -25,20 +25,20 @@ btnHamburger.addEventListener('click', function() {
 });
 
 
-//aboutme
-const aboutmeBtn = document.querySelector('#aboutmeBtn');
-const aboutmeTxt = document.querySelector('#aboutmetxt ');
-const experienceBtn = document.querySelector('#experienceBtn')
-const experiencetxt = document.querySelector('#experiencetxt');
+// //aboutme
+// const aboutmeBtn = document.querySelector('#aboutmeBtn');
+// const aboutmeTxt = document.querySelector('#aboutmetxt ');
+// const experienceBtn = document.querySelector('#experienceBtn')
+// const experiencetxt = document.querySelector('#experiencetxt');
 
-experienceBtn.addEventListener('click', function() {
-    aboutmeTxt.classList.add('.hidetxt ');
-    experiencetxt.classList.add('.showtxt ');
-});
-aboutmeBtn.addEventListener('click', function() {
-    if (experiencetxt.classList.contains('.showtxt ')) {
-        experiencetxt.classList.remove('.showtxt');
-        experiencetxt.classList.add('.hidetxt');
-        aboutmetxt.classList.add('.showtxt');
-    }
-});
+// experienceBtn.addEventListener('click', function() {
+//     aboutmeTxt.classList.add('.hidetxt ');
+//     experiencetxt.classList.add('.showtxt ');
+// });
+// aboutmeBtn.addEventListener('click', function() {
+//     if (experiencetxt.classList.contains('.showtxt ')) {
+//         experiencetxt.classList.remove('.showtxt');
+//         experiencetxt.classList.add('.hidetxt');
+//         aboutmetxt.classList.add('.showtxt');
+//     }
+// });
